3c52adf08cc88536e930d1e5c3d236473030fa1b
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-06-24 00:21:22 +0600

    fixed all linting error

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,56 +1,58 @@
 /* eslint-env node */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest/globals": true,
-        "cypress/globals": true
+  'root': true,
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jest/globals': true,
+    'cypress/globals': true,
+    "node": true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react', 'jest', 'cypress'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2, {
+        ignoredNodes: ["TemplateLiteral"]
+      }
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react", "jest", "cypress"
+    'linebreak-style': 0,
+    'quotes': [
+      'error',
+      'single'
     ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "eqeqeq": "error",
-        "no-trailing-spaces": "error",
-        "object-curly-spacing": [
-            "error", "always"
-        ],
-        "arrow-spacing": [
-            "error", { "before": true, "after": true }
-        ],
-        "no-console": 0,
-        "react/prop-types": 0
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'react/prop-types': 0
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
     }
+  },
+  'parser': 'babel-eslint'
 }
--- client/src/components/UsersPage.js
+++ client/src/components/UsersPage.js
@@ -6,28 +6,28 @@ import { Link } from 'react-router-dom'
 
 const UsersPage = () => {
   const users = useSelector((state) => state.users)
-    return (
-        <>
-            <h1 className={styles.users__title}>Users</h1>
+  return (
+    <>
+      <h1 className={styles.users__title}>Users</h1>
 
-            <div className={styles.users__container}>
-                <span className={styles.users__subtitleName}>username</span>
-                <span className={styles.users__subtitle}>blogs created</span>
-                <ul className={styles.users__list}>
-                    {users?.map((user) => (
-                        <li className={styles.users__listitem} key={user?.id}>
-                            <Link className={styles.users__link} to={`/users/${user.id}`}>
-                            <span className={styles.users__name}> {user?.username} </span>
-                            <span className={styles.users__bloglength}>
-                                {user?.blogs?.length}
-                            </span>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
+      <div className={styles.users__container}>
+        <span className={styles.users__subtitleName}>username</span>
+        <span className={styles.users__subtitle}>blogs created</span>
+        <ul className={styles.users__list}>
+          {users?.map((user) => (
+            <li className={styles.users__listitem} key={user?.id}>
+              <Link className={styles.users__link} to={`/users/${user.id}`}>
+                <span className={styles.users__name}> {user?.username} </span>
+                <span className={styles.users__bloglength}>
+                  {user?.blogs?.length}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export default UsersPage
